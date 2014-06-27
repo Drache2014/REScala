bd8db6384528f4f369b5808c85d7aab496d07e12
Drache2014 <zhaolong2014@gmail.com>
2014-06-27 02:08:00 +0200

    Create Demo2.scala

---
+++ mission1/Demo2.scala
@@ -0,0 +1,32 @@
+import swing._
+import swing.event._
+import rescala.commons.time._
+import scala.swing.Swing
+
+object Demo2 extends SimpleSwingApplication{
+  
+  val timer = Timer(100)
+  
+  def top = frame
+    
+    val frame = new MainFrame {
+    title = "Current time"
+    contents = new Panel{
+    	override def paintComponent(g: Graphics2D) {
+    	val current = Time.current.ms
+    	val seconds = Time.current.s.toInt
+    	val secsMod10 = (Time.current.s % 10).toInt
+	    g.drawString("The Time is " + current, 20, 20)
+	    g.drawString("The Time in seconds is " + seconds, 20, 40)
+	    g.drawString("The Time modulo 10 is " + secsMod10, 20, 60)
+      }
+    }
+    size = new Dimension(300, 120)
+  }
+  
+   override def main(args: Array[String]) {
+    super.main(args)
+    timer.tick += { _ => frame.repaint } 
+    Timer.runAll
+  }
+}
